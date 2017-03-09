348abd76627f6330fa32ee9187c149c580a1a7ea
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2017-03-09 15:58:03 +0100

    index and echo post

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-#!/home/okosarko/.conda/envs/my_root/bin/python
 from flask import Flask
 from flask import request
+from flask import jsonify
+
+import os
+
+port = os.environ.get('PORT', 5000)
+debug = os.environ.get('DEBUG', False)
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def index():
-    print(request.get_json(force=True))
-    return "Hellow, World!"
+    return "Hello, World!"
+
+@app.route('/payload', methods=['POST'])
+def payload():
+    json_in = request.get_json()
+    return jsonify(json_in)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=debug, port=port)
