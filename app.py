3073c240c0bf7f5e5394fbc08d5b537307f401fa
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2017-03-10 12:15:07 +0100

    always return something

--- app.py
+++ app.py
@@ -35,15 +35,15 @@ def add_label(issue_url, label):
     return "Got {}".format(r.status_code), r.status_code
 
 
-def process_merged_pr(payload):
-    action = payload.get('action', None)
-    pull_request = payload.get('pull_request', None)
+def process_merged_pr(json_payload):
+    action = json_payload.get('action', None)
+    pull_request = json_payload.get('pull_request', None)
     if pull_request and action == 'closed' and pull_request.merged:
         url = pull_request['url']
         label = 'Merged in dev'
-        add_label(url, label)
+        return add_label(url, label)
     else:
-        pass
+        return "Doing nothing"
 
 
 @app.route('/payload', methods=['POST'])
