2898c1d981714a807cff8f672d2ddcb4063d7ecf
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2017-11-16 10:51:50 +0100

    Remove 'Has PR' before adding 'Merged in dev' on the assigned issue
    Did we really need the sleep?

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import request
-#from flask import jsonify
 
 import json
 from pprint import pformat
@@ -14,10 +13,9 @@ import requests
 import os
 import logging
 import re
-from time import sleep
 
 port = int(os.environ.get('PORT', 5000))
-#logging.info('Listening on port {0}'.format(port))
+# logging.info('Listening on port {0}'.format(port))
 debug = os.environ.get('DEBUG', 'False') == 'True'
 secret = os.environ.get('SECRET').encode('utf8')
 headers = {'Authorization': 'token ' + os.environ.get('GITHUB_API_KEY')}
@@ -37,14 +35,27 @@ def index():
 
 def add_label(*args, **kwargs):
     response = ""
-    status = 0
+    status = 200
     for issue_url in args:
         if issue_url:
             r = requests.post(issue_url + '/labels', json=[kwargs['label']], headers=headers)
             logging.debug(pformat(r))
             response += "{}: Got {}".format(issue_url, r.status_code)
             status = r.status_code if r.status_code > status else status
-    return response, r.status_code
+    return response, status
+
+
+def remove_label(issue_url, label):
+    response = ""
+    status = 200
+    if issue_url:
+        r = requests.delete(issue_url + '/labels/' + label,  headers=headers)
+        logging.debug(pformat(r))
+        response += "{}: Got {}".format(issue_url, r.status_code)
+        status = r.status_code if r.status_code > status else status
+    if status >= 300:
+        logging.error(pformat(r))
+    return response, status
 
 
 def process_merged_pr(json_payload):
@@ -54,15 +65,14 @@ def process_merged_pr(json_payload):
     if pull_request and action == 'closed' and\
             pull_request['merged'] and pull_request['base']['label'] == dev_branch_name:
         pull_request_url = pull_request['issue_url']
-        #assume the pr branch name has an issue number assigned...like issue_#123
+        # assume the pr branch name has an issue number assigned...like issue_#123
         assigned_issue_url = None
         m = re.search('\D*#(\d+)\D*', pull_request['head']['ref'])
         if m:
             issues_url = json_payload.get('repository')['issues_url']
             assigned_issue_url = issues_url.format(**{'/number': '/' + m.group(1)})
         label = 'Merged in dev'
-        #naive wait for other hooks to do their stuff
-        sleep(5)
+        remove_label(assigned_issue_url, 'Has PR')
         return add_label(pull_request_url, assigned_issue_url, label=label)
     else:
         return "Doing nothing"
